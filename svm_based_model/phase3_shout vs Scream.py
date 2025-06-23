2631a11534bee8ca0c6d5e40ad61915cdbd3503a
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-24 01:19:12 +0530

    Use pandas for CSV loading in SVM scripts

--- svm_based_model/phase3_shout vs Scream.py
+++ svm_based_model/phase3_shout vs Scream.py
@@ -1,10 +1,8 @@
 from sklearn import svm
-import numpy as np
-from numpy import genfromtxt
 import pandas as pd
 from sklearn.metrics import accuracy_score
 import pickle
-x_train = genfromtxt('pathlocation of csv',delimiter=',')
-y_train = genfromtxt('id_of_it  csv',delimiter=',')
+x_train = pd.read_csv('pathlocation of csv', header=None).values
+y_train = pd.read_csv('id_of_it  csv', header=None).values.ravel()
 third_para = svm.SVC(kernel='linear',C=20.0,gamma=0.00001)
 third_para.fit(x_train, y_train)
